9803ac2248b655ee2002165b3c6059f9c3c77db5
Joby M. Anthony III <jmanthony1@liberty.edu>
2025-03-05 11:29:58 -0500

    temporarily take out SymmetricTensors; remove duck typing for Optimization; #2

--- src/BammannChiesaJohnsonPlasticity.jl
+++ src/BammannChiesaJohnsonPlasticity.jl
@@ -6,9 +6,18 @@ using Reexport
 
 
 using Tensors # : *, ⊡, sqrt, dev
-export δ, vonMises
+export δ, vonMises, symmetricmagnitude, symmetricvonMises
 δ(i, j) = i == j ? 1.0 : 0.0 # helper function
-vonMises(x) = (s = dev(x); sqrt(3.0/2.0 * s ⊡ s))
+# vonMises(x) = (s = dev(x); sqrt(3.0/2.0 * s ⊡ s))
+symmetricmagnitude(tensor::Vector{<:Real}) = √( sum(tensor[1:3] .^ 2.) + 2sum(tensor[4:6] .^ 2.) )
+
+function symmetricvonMises(tensor::Union{Vector{<:Real}, SubArray{<:Real}})::AbstractFloat
+    σvM = sum(map(x->x^2., [tensor[1] - tensor[2], tensor[2] - tensor[3], tensor[3] - tensor[1]])) + (
+        6sum(map(x->x^2., [tensor[4], tensor[5], tensor[6]])))
+    return √(σvM / 2.)
+end
+
+symmetricvonMises(tensor::Matrix{<:Real})::Vector{AbstractFloat} = map(symmetricvonMises, eachcol(tensor))
 
 
 export AbstractBCJ
