0ecc696fc13690716ef8e27d24ce5d0b3bbb0393
Joby M. Anthony III <jmanthony1@liberty.edu>
2025-03-12 16:34:39 -0400

    simplify syntax in test (#7)

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,72 +12,43 @@ using Test
 
 
 @testset verbose=true "BammannChiesaJohnsonPlasticity.jl" begin
-    params      = begin
-        df          = CSV.read("Props_BCJ_4340_fit.csv", DataFrame; header=true, delim=',', types=[String, Float64])
-        rowsofconstants = findall(occursin.(r"C\d{2}", df[!, "Comment"]))
-        C_0         = Vector{Float64}(undef, 20)
-        C_0[rowsofconstants] .= df[!, "For Calibration with vumat"][rowsofconstants]
-        bulk_mod    = df[!, "For Calibration with vumat"][findfirst(occursin("Bulk Mod"), df[!, "Comment"])]
-        shear_mod   = df[!, "For Calibration with vumat"][findfirst(occursin("Shear Mod"), df[!, "Comment"])]
-        ( # collect as dictionary
-            C₁  = C_0[ 1],
-            C₂  = C_0[ 2],
-            C₃  = C_0[ 3],
-            C₄  = C_0[ 4],
-            C₅  = C_0[ 5],
-            C₆  = C_0[ 6],
-            C₇  = C_0[ 7],
-            C₈  = C_0[ 8],
-            C₉  = C_0[ 9],
-            C₁₀ = C_0[10],
-            C₁₁ = C_0[11],
-            C₁₂ = C_0[12],
-            C₁₃ = C_0[13],
-            C₁₄ = C_0[14],
-            C₁₅ = C_0[15],
-            C₁₆ = C_0[16],
-            C₁₇ = C_0[17],
-            C₁₈ = C_0[18],
-            C₁₉ = C_0[19],
-            C₂₀ = C_0[20],
-            G   = bulk_mod,
-            μ   = shear_mod)
-    end
     df_Tension_e002_295 = CSV.read("Data_Tension_e0002_T295.csv", DataFrame;
         header=true, delim=',', types=[Float64, Float64, Float64, Float64, String])
     test = BCJMetalUniaxialTest(df_Tension_e002_295[!, "Strain"], df_Tension_e002_295[!, "Stress"] .* 1e6, name="exp")
     bcj_loading = BCJMetalStrainControl(295.0, 2e-3, float(last(df_Tension_e002_295[!, "Strain"])), 200, :tension)
-    ψ = DK(bcj_loading, params.μ)
+    G = 159e9   # shear modulus [Pa]
+    μ = 77e9    # bulk modulus [Pa]
+    ψ = DK(bcj_loading, μ)
     p = ComponentVector(
-        C₁  = params.C₁,    C₂  = params.C₂,    # V
-        C₃  = params.C₃,    C₄  = params.C₄,    # Y
-        C₅  = params.C₅,    C₆  = params.C₆,    # f
-        C₇  = params.C₇,    C₈  = params.C₈,    # r_d
-        C₉  = params.C₉,    C₁₀ = params.C₁₀,   # h
-        C₁₁ = params.C₁₁,   C₁₂ = params.C₁₂,   # r_s
-        C₁₃ = params.C₁₃,   C₁₄ = params.C₁₄,   # R_d
-        C₁₅ = params.C₁₅,   C₁₆ = params.C₁₆,   # H
-        C₁₇ = params.C₁₇,   C₁₈ = params.C₁₈,   # R_s
-        C₁₉ = params.C₁₉,   C₂₀ = params.C₂₀    # Y_adj
+        C₁  = 35016459.896579415,       C₂  = 323.93342698083165,   # V
+        C₃  = 500340419.8337271,        C₄  = 143.08381901004486,   # Y
+        C₅  = 4.101775377562497,        C₆  = 271.0245526,          # f
+        C₇  = 1.0834796217232945e-06,   C₈  = 1023.6278003945317,   # r_s
+        C₉  = 2358205093.844017,        C₁₀ = 676421.9935474312,    # h
+        C₁₁ = 1.3465080192134937e-10,   C₁₂ = 98.35671405000001,    # r_d
+        C₁₃ = 2.533629073577668e-09,    C₁₄ = 403.2291451343492,    # R_s
+        C₁₅ = 1159915808.5023918,       C₁₆ = 959557.0948847248,    # H
+        C₁₇ = 6.204370386543724e-12,    C₁₈ = 203.95288011132806,   # R_s
+        C₁₉ = 1e-10,                    C₂₀ = 1e-10                 # Y_adj
     )
     res = ContinuumMechanicsBase.predict(ψ, test, p)
     q = ComponentVector(
-        C₁  = NaN,          C₂  = params.C₂,    # V
-        C₃  = params.C₃,    C₄  = params.C₄,    # Y
-        C₅  = params.C₅,    C₆  = params.C₆,    # f
-        C₇  = params.C₇,    C₈  = params.C₈,    # r_d
-        C₉  = params.C₉,    C₁₀ = params.C₁₀,   # h
-        C₁₁ = params.C₁₁,   C₁₂ = params.C₁₂,   # r_s
-        C₁₃ = params.C₁₃,   C₁₄ = params.C₁₄,   # R_d
-        C₁₅ = params.C₁₅,   C₁₆ = params.C₁₆,   # H
-        C₁₇ = params.C₁₇,   C₁₈ = params.C₁₈,   # R_s
-        C₁₉ = params.C₁₉,   C₂₀ = params.C₂₀    # Y_adj
+        C₁  = NaN,      C₂  = p.C₂,    # V
+        C₃  = p.C₃,     C₄  = p.C₄,    # Y
+        C₅  = p.C₅,     C₆  = p.C₆,    # f
+        C₇  = p.C₇,     C₈  = p.C₈,    # r_d
+        C₉  = p.C₉,     C₁₀ = p.C₁₀,   # h
+        C₁₁ = p.C₁₁,    C₁₂ = p.C₁₂,   # r_s
+        C₁₃ = p.C₁₃,    C₁₄ = p.C₁₄,   # R_d
+        C₁₅ = p.C₁₅,    C₁₆ = p.C₁₆,   # H
+        C₁₇ = p.C₁₇,    C₁₈ = p.C₁₈,   # R_s
+        C₁₉ = p.C₁₉,    C₂₀ = p.C₂₀    # Y_adj
     )
     prob = BCJPlasticityProblem(ψ, test, p; ad_type=AutoForwardDiff(), ui=q)
     sol = solve(prob, LBFGS())
     @test sol.retcode == SciMLBase.ReturnCode.Success
 
-    # # bcj_loading = BCJ_metal(295., 570., 0.15, 200, 1, params)
+    # # bcj_loading = BCJ_metal(295., 570., 0.15, 200, 1, p)
     # bcj_conf_Tension_e002_295       = kernel(DK, bcj_loading_Tension_e002_295)
     # bcj_ref_Tension_e002_295        = bcj_conf_Tension_e002_295[1]
     # bcj_current_Tension_e002_295    = bcj_conf_Tension_e002_295[2]
@@ -108,8 +79,8 @@ using Test
 
     # df_Tension_e570_295 = CSV.read("Data_Tension_e570_T295.csv", DataFrame;
     #     header=true, delim=',', types=[Float64, Float64, Float64, Float64, String])
-    # # bcj_loading = BCJMetalStrainControl(295., 2e-3, 1., 200, 1, params)
-    # bcj_loading_Tension_e570_295 = BCJMetalStrainControl(295., 570., 0.15, 200, :tension, params)
+    # # bcj_loading = BCJMetalStrainControl(295., 2e-3, 1., 200, 1, p)
+    # bcj_loading_Tension_e570_295 = BCJMetalStrainControl(295., 570., 0.15, 200, :tension, p)
     # bcj_conf_Tension_e570_295 = referenceconfiguration(DK, bcj_loading_Tension_e570_295)
     # bcj_ref_Tension_e570_295        = bcj_conf_Tension_e570_295[1]
     # bcj_current_Tension_e570_295    = bcj_conf_Tension_e570_295[2]
